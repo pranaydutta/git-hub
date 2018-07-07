5e03e1455bcd7be708fc36c94053b0d01e302645
unknown <pranaykumardutta@gmail.com>
2018-07-07 15:08:40 +0530

    modified if check data

--- src/main/java/github/git/hub/App.java
+++ src/main/java/github/git/hub/App.java
@@ -6,6 +6,16 @@ package github.git.hub;
  */
 public class App 
 {
+	
+	public void addData(String data)
+	{
+		System.out.println("data="+data);
+		if(data.equals("Pranay"))
+		{
+			System.out.println("welcome to"+data);
+			
+		}
+	}
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
